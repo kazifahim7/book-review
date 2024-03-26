e7a217c689fd27813e4de90bbb051d726b75a9e3
kazi fahim <kazifahim661@gmail.com>
2024-03-26 11:32:52 +0600

    book read list done

--- src/components/ReadStore.jsx
+++ src/components/ReadStore.jsx
@@ -6,7 +6,7 @@ import { RiProfileLine } from "react-icons/ri";
 const ReadStore = ({ book }) => {
     return (
         <div>
-            <div className="card card-side bg-base-100 shadow-xl">
+            <div className="card card-side bg-base-100 shadow-xl mb-2">
                 <figure><img src={book.image} alt="Movie" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{book.bookName}</h2>
@@ -23,17 +23,21 @@ const ReadStore = ({ book }) => {
                     </div>
 
                     <hr />
-                    
 
+                    <div className='space-x-4 flex flex-wrap  gap-2'>
+                        <button className="btn btn-outline btn-success">Category: {book.category}</button>
+                        <button className="btn btn-outline btn-success">Rating: {book.rating}</button>
+                        <button className="btn btn-outline btn-success">View Details</button>
 
-
-
-
-
-                    
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
                     </div>
+
+
+
+
+
+
+
+
                 </div>
             </div>
 
